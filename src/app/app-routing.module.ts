28101ac7824ea1efd07b74601e6b8356561c7710
giovdellap <giovdellap@gmail.com>
2023-10-03 21:48:25 +0200

    Minor commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './pages/homepage/homepage.component';
 import { LoginpageComponent } from './pages/loginpage/loginpage.component';
+import { ProductsComponent } from './pages/products/products.component';
 
 
 const routes: Routes = [
   {path: '', component: HomepageComponent},
-  {path: 'login', component: LoginpageComponent}
+  {path: 'login', component: LoginpageComponent},
+  {path: 'products', component: ProductsComponent}
 ];
 
 
--- src/app/pages/products/components/add/add.component.ts
+++ src/app/pages/products/components/add/add.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
   styleUrls: ['./add.component.css']
 })
+
 export class AddComponent {
+
+  productGroup = new FormGroup({
+    title: new FormControl(''),
+    description: new FormControl(''),
+    price: new FormControl(0),
+    quantity: new FormControl('false')
+  });
+
+
 
 }
